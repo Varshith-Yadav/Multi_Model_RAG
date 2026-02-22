283546433153099e1eee11b0a5dfac3ed6934da3
Varshith-Yadav <varshithlavanya67@gmail.com>
2026-02-22 18:39:09 +0530

    Generatin Response with ollama:llama3

---
+++ RAG/generation/llm.py
@@ -0,0 +1,11 @@
+import requests 
+def generate(prompt):
+    res = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": 'llama3',
+            "prompt": prompt,
+            "stream": False
+        }
+    )
+    return res.json()['response']
